bab5df0cff6c0f8c76d6fb4348c4d77701a15c0a
kgh3489 <kgh3489@naver.com>
2022-09-20 10:18:08 +0900

    메인페이지 리스트API 생성

---
+++ src/main/java/com/group2/scheduler/service/LectureRepository.java
@@ -0,0 +1,9 @@
+package com.group2.scheduler.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.group2.scheduler.entity.LectureEntity;
+
+public interface LectureRepository extends JpaRepository<LectureEntity, Integer>{
+
+}
---
+++ src/main/java/com/group2/scheduler/service/LectureService.java
@@ -0,0 +1,10 @@
+package com.group2.scheduler.service;
+
+import java.util.List;
+
+import com.group2.scheduler.entity.LectureEntity;
+
+public interface LectureService {
+
+	List<LectureEntity> getList(); //모든 목록
+}
