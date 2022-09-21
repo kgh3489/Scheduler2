b5d216263bfce20e0bf8e92b99afd2101e7196fc
kgh3489 <kgh3489@naver.com>
2022-09-21 15:42:19 +0900

    수강 신청 시 수강 인원 증가 API 생성

--- src/main/java/com/group2/scheduler/controller/LectureController.java
+++ src/main/java/com/group2/scheduler/controller/LectureController.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.group2.scheduler.entity.LectureEntity;
-import com.group2.scheduler.entity.RegisterEntity;
-import com.group2.scheduler.entity.UserEntity;
 import com.group2.scheduler.service.LectureService;
+import com.group2.scheduler.service.RegisterService;
 
 @Controller
 @RequestMapping("/lecture")
@@ -25,6 +23,10 @@ public class LectureController {
 	@Autowired
 	@Qualifier("lectureService")
 	private LectureService lectureService;
+	
+	@Autowired
+	@Qualifier("registerService")
+	private RegisterService registerService;
 	
 	//강의 리스트
 	@GetMapping("/list")
@@ -42,6 +44,11 @@ public class LectureController {
 		
 		LectureEntity lecture = lectureService.getDetail(lno);
 		model.addAttribute("lecture", lecture);
+		
+		//수강 인원
+		int fixedNum = registerService.CountUp(lecture);
+		model.addAttribute("fixedNum", fixedNum);
+		
 		return "lecture/detail";
 	}
 	
