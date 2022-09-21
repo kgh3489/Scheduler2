7d596578534ac8c2209fc97f96bb26168eb406a1
kyuri-L <julie7303@gmail.com>
2022-09-22 06:03:47 +0900

    id 중복 유효성 검사

--- src/main/java/com/group2/scheduler/service/UserRepository.java
+++ src/main/java/com/group2/scheduler/service/UserRepository.java
@@ -1,10 +1,13 @@
 package com.group2.scheduler.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.group2.scheduler.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer>{
 
-	UserEntity findByIdAndPw(String id, String pw);
+	@Query("select u from UserEntity u where u.id = :id")
+	UserEntity checkUserInfo(@Param("id") String id);
 }
